432d5cf565df2150c63fccb2edef6eb11080ba1b
Mark Atherton <mark@insightinternet.co.uk>
2021-10-19 13:32:37 +0100

    end of lesson 152

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 function App() {
   const [showPara, setShowPara] = useState(false);
+  console.log("App Running!!");
   const togglePara = () => {
     setShowPara((prevShowPara) => !prevShowPara);
   };
