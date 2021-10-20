77386ef4f6129182aa3f4c5dbebdf7bc11cdef0d
Mark Atherton <mark@insightinternet.co.uk>
2021-10-20 11:26:57 +0100

    end of lesson 155: wrapped useCallBack around setShowPara method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
 
@@ -7,14 +7,16 @@ import "./App.css";
 function App() {
   const [showPara, setShowPara] = useState(false);
   console.log("App Running!!");
-  const togglePara = () => {
+
+  const togglePara = useCallback(() => {
     setShowPara((prevShowPara) => !prevShowPara);
-  };
+  }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <Button onClick={togglePara}>Toggle</Button>
-      <DemoOutput show={showPara} />
+      <DemoOutput show={false} />
     </div>
   );
 }
